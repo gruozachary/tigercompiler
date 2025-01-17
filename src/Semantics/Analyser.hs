e81aa5be3148dae4b02c6b7fd4b6017fadd0163b
Lukas <lukastrakim@gmail.com>
2025-01-17 15:04:19 +0000

    Type checking workflow

--- src/Semantics/Analyser.hs
+++ src/Semantics/Analyser.hs
@@ -2,19 +2,19 @@ module Semantics.Analyser
     (
     ) where
 
-import Semantics.SymbolTable (SymbolTable (new))
+import Semantics.SymbolTable (SymbolTable (new, fromList))
 import qualified Parsing.Nodes as N
-import Control.Monad.State (State)
+import Control.Monad.State (StateT, lift, gets, modify)
 import Control.Monad (void)
 
 data Ty
     = TInt
     | TString
-    | TRecord [(String, Ty)] -- *
-    | TArray Ty              -- *
-    | Nil
-    | Unit
-    | Name String (Maybe Ty) -- *
+    | TRecord [(String, Ty)] Int
+    | TArray Ty Int
+    | TNil
+    | TUnit
+    | TName String (Maybe Ty) deriving Eq
 
     -- * might need some sort of unique identifier
 
@@ -29,18 +29,57 @@ type Expty = (Exp, Ty)
 eVenv :: (SymbolTable t) => t EnvEntry
 eTenv :: (SymbolTable t) => t Ty
 eVenv = new
-eTenv = new
+eTenv = fromList [("string", TString), ("int", TInt)]
 -- empty venv and tenv
 
 data Data t = Data
     { currentEnv :: t EnvEntry
     , currentTyEnv :: t Ty
+    , nextId :: Int
     }
 
-type Analyser t a = State (Data t) a
+type Error = String
+type Analyser t a = StateT (Data t) (Either Error) a
+
+getNextId :: Analyser t Int
+getNextId = do
+    i <- gets nextId
+    modify (\s -> s { nextId = i + 1 })
+    return i
+
+
+transTy :: (SymbolTable t) => N.Type -> Analyser t Ty
+transTy = undefined
 
 transExpr :: (SymbolTable t) => N.Expr -> Analyser t Expty
-transExpr = undefined
+transExpr N.NilEx = return ((), TNil)
+transExpr (N.IntEx _) = return ((), TInt)
+transExpr (N.StrEx _) = return ((), TString)
+transExpr (N.ArrayEx tid size element) = do
+    arrayT <- transTy (N.IdTy tid)
+    (et, i) <- case arrayT of
+        (TArray t i) -> return (t, i)
+        _ -> lift (Left "type of array is not real")
+    case size of
+        N.IntEx _ -> return ()
+        _ -> lift (Left "size of array is not an integer literal")
+    elementT <- transExpr element
+    if snd elementT == et
+        then return ((), TArray et i) 
+        else lift (Left "array type does not match initial elements")
+transExpr (N.RecordEx tid entries) = undefined
+    -- t <- transTy (N.IdTy tid)
+transExpr (N.LValEx _) = undefined
+transExpr (N.FunCallEx _ _ ) = undefined
+transExpr (N.NegEx _ ) = undefined
+transExpr (N.OpEx _ _ _) = undefined
+transExpr (N.Exs _) = undefined
+transExpr (N.AssignEx _ _ ) = undefined
+transExpr (N.IfEx _ _ _) = undefined
+transExpr (N.WhileEx _ _) = undefined
+transExpr (N.ForEx _ _ _ _) = undefined
+transExpr (N.BreakEx) = undefined
+transExpr (N.LetEx _ _) = undefined
 
 transChunk :: (SymbolTable t) => N.Chunk -> Analyser t ()
 transChunk = undefined
--- src/Semantics/SymbolTable.hs
+++ src/Semantics/SymbolTable.hs
@@ -1,6 +1,6 @@
 {-# LANGUAGE DeriveFunctor #-}
 module Semantics.SymbolTable
-    ( SymbolTable, new, insert, look, entries, add
+    ( SymbolTable, new, insert, look, entries, add, fromList
     ) where
 
 import qualified Data.Map as Map
@@ -8,13 +8,21 @@ import qualified Data.Map as Map
 class SymbolTable t where
     -- provides an empty table
     new :: t a
+    
+    -- initialise a symbol table from a list of bindings
+    fromList :: [(String, a)] -> t a
+    fromList = foldr (\(k, x) m -> insert m k x) new
+    
     -- inserts an element into the table, OVERWRITING existing elements if the
     -- Stringey already exists
     insert :: t a -> String -> a -> t a
+    
     -- returns the value associated with a Stringey
     look :: t a -> String -> Maybe a
+    
     -- returns the set of entries in the table
     entries :: t a -> [(String, a)]
+    
     -- combines two symbol tables, existing elements in t0 being overwritten by
     -- elements in t1
     add :: t a -> t a -> t a
