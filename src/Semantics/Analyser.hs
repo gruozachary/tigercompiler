82216da4726580108d8098e18a9503757583f23e
Thomas Zachary <thomaszachary06@proton.me>
2025-01-20 21:17:55 +0000

    refactored Analyser into Analyser and Semant

--- src/Semantics/Analyser.hs
+++ src/Semantics/Analyser.hs
@@ -1,11 +1,9 @@
 module Semantics.Analyser
-    (
+    ( Ty(..), EnvEntry(..), Exp, Expty, Data(..), Error, Analyser
+    , getNextId, matchTy, expectInt, expectString, expectRecord, expectArray
+    , expectNil, expectUnit, expectName
     ) where
-
-import Semantics.SymbolTable (SymbolTable (new, fromList))
-import qualified Parsing.Nodes as N
-import Control.Monad.State (StateT, lift, gets, modify)
-import Control.Monad (void)
+import Control.Monad.State (StateT, gets, modify, MonadTrans (lift))
 
 data Ty
     = TInt
@@ -23,12 +21,6 @@ data EnvEntry
 
 type Exp = () -- TODO add implementation (intermediate code generation stage)
 type Expty = (Exp, Ty)
-
-eVenv :: (SymbolTable t) => t EnvEntry
-eTenv :: (SymbolTable t) => t Ty
-eVenv = new
-eTenv = fromList [("string", TString), ("int", TInt)]
--- empty venv and tenv
 
 data Data t = Data
     { currentEnv :: t EnvEntry
@@ -72,40 +64,3 @@ expectUnit _ TUnit = return ()
 expectUnit e _ = lift (Left e)
 expectName _ (TName x y) = return (x, y)
 expectName e _ = lift (Left e)
-
-transTy :: (SymbolTable t) => N.Type -> Analyser t Ty
-transTy = undefined
-
-transExpr :: (SymbolTable t) => N.Expr -> Analyser t Expty
-transExpr N.NilEx = return ((), TNil)
-transExpr (N.IntEx _) = return ((), TInt)
-transExpr (N.StrEx _) = return ((), TString)
-transExpr (N.ArrayEx tid size element) = do
-    t <- transTy (N.IdTy tid)
-    (et, _) <- expectArray "type of array is not defined" t
-    case size of
-        N.IntEx _ -> return ()
-        _ -> lift (Left "size of array is not an integer literal")
-    (_, et') <- transExpr element
-    matchTy "array type does not match element initialiser" et et'
-    return ((), t)
-transExpr (N.RecordEx tid entries) = undefined
-transExpr (N.LValEx _) = undefined 
-transExpr (N.FunCallEx _ _ ) = undefined
-transExpr (N.NegEx _ ) = undefined
-transExpr (N.OpEx _ _ _) = undefined
-transExpr (N.Exs _) = undefined
-transExpr (N.AssignEx _ _ ) = undefined
-transExpr (N.IfEx _ _ _) = undefined
-transExpr (N.WhileEx _ _) = undefined
-transExpr (N.ForEx _ _ _ _) = undefined
-transExpr (N.BreakEx) = return ((), TUnit)
-transExpr (N.LetEx _ _) = undefined
-
-transChunk :: (SymbolTable t) => N.Chunk -> Analyser t ()
-transChunk = undefined
-
-analyse :: (SymbolTable t) => N.Program -> Analyser t ()
-analyse (N.ExprProg e)       = void (transExpr e)
-analyse (N.ChunkProg [])     = return ()
-analyse (N.ChunkProg (c:cs)) = transChunk c >> analyse (N.ChunkProg cs)
